c95b60f91a3ab2609224cc24f3cc5172aa036bb0
Elizabeth Karst <lkarst@outlook.com>
2018-08-18 21:28:57 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,2 +1,16 @@
-def prime?()
+def prime?(n)
+  i = 1
+  count = 0
+
+  while i <=n 
+    if n % i == 0
+      count += 1
+      i += 1
+    else
+      i += 1
+    end
+  end
+
+  count == 2 ? true : false
+  
 end
